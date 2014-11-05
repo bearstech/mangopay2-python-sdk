647ac71e14ccd9dce2460a232b1b966cb34a5211
Encolpe DEGOUTE <edegoute@bearstech.com>
2014-11-05 15:46:26 +0100

    Allow usage of 'python2.7 setup.py develop'

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # to build - python setup.py sdist upload
 setup(
